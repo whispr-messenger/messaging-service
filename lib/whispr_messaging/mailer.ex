ea421887b9a0595e170472d4e12848551a5e5e85
Roadmvn <tudy.gbaguidi@epitech.eu>
2025-09-22 01:23:37 +0200

    Removes messaging service codebase
    This commit removes the entire codebase for the messaging service.
    This indicates a significant architectural change, such as a move to a different technology stack,
    outsourcing the service, or a complete discontinuation of the messaging functionality.

--- lib/whispr_messaging/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule WhisprMessaging.Mailer do
-  use Swoosh.Mailer, otp_app: :whispr_messaging
-end
--- lib/whispr_messaging/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule WhisprMessaging.Repo do
-  use Ecto.Repo,
-    otp_app: :whispr_messaging,
-    adapter: Ecto.Adapters.Postgres
-end
