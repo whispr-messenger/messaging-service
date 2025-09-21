79ac677de5b573b3e39a22725da20e7021d79567
Roadmvn <tudy.gbaguidi@epitech.eu>
2025-09-22 01:23:37 +0200

    Removes messaging service codebase
    This commit removes the entire codebase for the messaging service.
    This indicates a significant architectural change, such as a move to a different technology stack,
    outsourcing the service, or a complete discontinuation of the messaging functionality.

--- lib/whispr_messaging_web/channels/conversation_presence.ex
+++
@@ -1,8 +0,0 @@
-defmodule WhisprMessagingWeb.ConversationPresence do
-  @moduledoc """
-  Module Presence pour tracker la présence dans les conversations individuelles
-  """
-  use Phoenix.Presence,
-    otp_app: :whispr_messaging,
-    pubsub_server: WhisprMessaging.PubSub
-end
--- lib/whispr_messaging_web/channels/presence.ex
+++
@@ -1,8 +0,0 @@
-defmodule WhisprMessagingWeb.Presence do
-  @moduledoc """
-  Module Presence pour tracker la présence globale des utilisateurs
-  """
-  use Phoenix.Presence,
-    otp_app: :whispr_messaging,
-    pubsub_server: WhisprMessaging.PubSub
-end
