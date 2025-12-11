f5d28690bd717ba3abdc9fbb631b5e206d63ed7b
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-12-11 19:55:13 +0100

    docker: cleanup docker configuration

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WhisprMessaging.MixProject do
     [
       app: :whispr_messaging,
       version: "1.0.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
